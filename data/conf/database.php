367621a8e462c37f9b2cfed3af058c92800583dc
ewuioq <752641024@qq.com>
2025-05-04 13:29:45 +0000

    全新提交：已彻底移除敏感数据

---
+++ data/conf/database.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 配置文件
+ */
+
+return [
+    // 数据库类型
+    'type'     => 'mysql',
+    // 服务器地址
+    'hostname' => '127.0.0.1',
+
+	
+    // 数据库名
+    'database' => 'sql_admin_hbhkkl',
+    // 用户名
+    'username' => 'sql_admin_hbhkkl',
+    // 密码
+    'password' => '9d8b2285d15b18',
+    // 端口
+    'hostport' => '3306',
+    // 数据库编码默认采用utf8
+    'charset'  => 'utf8mb4',
+    // 数据库表前缀
+    'prefix'   => 'cmf_',
+    "authcode" => 'JL5rPyAVz3wy8rKq3F',
+    //#COOKIE_PREFIX#
+];
